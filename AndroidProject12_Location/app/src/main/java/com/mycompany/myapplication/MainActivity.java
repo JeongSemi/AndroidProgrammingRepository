995e38e27d3bae3e0c2ebc0c85b56260c03defe6
JeongSemi <wjdtpa2@gmail.com>
2017-07-07 17:47:47 +0900

    7/7

--- AndroidProject12_Location/app/src/main/java/com/mycompany/myapplication/MainActivity.java
+++ AndroidProject12_Location/app/src/main/java/com/mycompany/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.mycompany.myapplication;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Path;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,12 +14,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 import org.apache.commons.lang.StringEscapeUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -29,11 +42,17 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private LocationManager locationManager;
+    private SupportMapFragment mapFragment;
+    private GoogleMap googleMap;
+    private List<LatLng> listLocation = new ArrayList<>();
+    private Polyline polyline;
+    private LatLng currLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         checkPermission();
     }
 
@@ -69,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "위도: " + latitude);
             Log.i(TAG, "경도: " + longitude);
             locationManager.removeUpdates(locationListener);
+            showMap(latitude, longitude);
         }
 
         @Override
@@ -84,6 +104,38 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void showMap(double latitude, double longitude) {
+        currLatLng = new LatLng(latitude, longitude);
+
+        //지도를 드로잉하기 위한 코드
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currLatLng, 16));
+        googleMap.setMapType(googleMap.MAP_TYPE_NORMAL);
+        checkPermission();
+        googleMap.setMyLocationEnabled(true);
+
+        //경로를 드로잉하기 위한 코드
+        listLocation.clear();
+        listLocation.add(currLatLng);
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                listLocation.add(latLng);
+                drawingPath();
+            }
+        });
+
+    }
+
+    public void drawingPath() {
+        if (polyline != null)
+            polyline.remove();
+        LatLng[] paths = new LatLng[listLocation.size()];
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .add(listLocation.toArray(paths))
+                .width(10)
+                .color(Color.RED);
+        polyline = googleMap.addPolyline(polylineOptions);
+    }
 
     public void handleBtnFromAddressToLocation(View v) {
         String url = "https://apis.daum.net/local/geo/addr2coord";
@@ -110,8 +162,8 @@ public class MainActivity extends AppCompatActivity {
                     jsonObject = jsonArray.getJSONObject(0);
                     double latitude = jsonObject.getDouble("lat");
                     double longitude = jsonObject.getDouble("lng");
-                    Log.i(TAG, "위도: "+ latitude);
-                    Log.i(TAG, "경도: "+ longitude);
+                    Log.i(TAG, "위도: " + latitude);
+                    Log.i(TAG, "경도: " + longitude);
                 } catch (JSONException e) {
                 }
             }
@@ -122,4 +174,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void handleBtnMap(View v) {
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                MainActivity.this.googleMap = googleMap;
+
+                handleBtnCurrLocation(null);
+            }
+        });
+    }
+
+
+    public void handleBtnCurrLocationReturn(View v) {
+        listLocation.add(currLatLng);
+        drawingPath();
+    }
+
+    public void handleBtnPathRemove(View v) {
+        listLocation.clear();
+        listLocation.add(currLatLng);
+        polyline.remove();
+    }
+
 }
